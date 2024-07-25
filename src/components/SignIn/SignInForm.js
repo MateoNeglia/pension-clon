71bd783c5a14918baff38ccea1d8fe5f84e1d846
nicolaslema <nicolemadev@gmail.com>
2024-07-25 14:29:22 -0300

    Componente formulario SingIn

--- src/components/SignIn/SignInForm.js
+++ src/components/SignIn/SignInForm.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import './SignInForm.scss'
+
+
 
 function SignInForm() {
   return (
-    <div>SignInForm</div>
+    <div className='wrapper u-background-light-blue'>
+      <form className='form-component ' action=''>
+        <h1>Sign in</h1>
+        <h4>Bienvenidos a Criteria Pension & Benefits</h4>
+        <span className='text-divider' >ingresa tus datos</span>
+        <div className='input-box'>
+          <input type='text' placeholder='Email'></input>
+        </div>
+        <div className='input-box '>
+          <input type='password' placeholder='Password'></input>
+        </div>
+        <div className='forgot-password'>
+          <a href='#'>Olvidaste tu contraseña?</a>
+        </div>
+        {/* hacer boton componente para reutilizar */}
+        <button type='sumbit'>Ingresar</button>
+      </form>
+    </div>
   )
 }
 
--- src/components/SignIn/SignInMain.js
+++ src/components/SignIn/SignInMain.js
@@ -7,11 +7,11 @@ function SignInMain() {
 <div className='sup-main-container'>
     <div className='main-container'>
         <div className='container'>
-            <div className='titulo-text u-titles'> Criteria | Pension & Benefits</div>
+            <div className='titulo-text u-titles'> Criteria <span className='titulo-decorator'>|</span> Pension & Benefits</div>
             <div className='form-container'> <SignInForm/></div>
         </div>
     </div>
-    <div className='bottom-container '>@ 2024 | Pension & benefits</div>
+    <div className='bottom-container '>@ 2024 Criteria | Pension & benefits</div>
 </div>
   )
 }

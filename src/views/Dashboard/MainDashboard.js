d27a67a56d5cb5cb188b7af03ac78d01e87cb091
nicolaslema <nicolemadev@gmail.com>
2024-08-19 16:24:38 -0300

    Added Dashboard Grid. Added SectionTwo.

---
+++ src/views/Dashboard/MainDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SubSectionTwo  from './subSectionTwo/subSectionTwo'
+import "./MainDashboard.scss"
+
+export default function MainDashboard() {
+  return (
+    <div className='u-displayGrid section-two-main'>
+      
+       <div className=''><SubSectionTwo/></div>
+
+    </div>
+  )
+}
---
+++ src/views/Dashboard/subSectionTwo/subSectionTwo.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './subSectionTwo.scss'
+import { CardOneItems } from '../../../components/SubSectionTwo/CardOne/CardOneItems';
+import AttachMoneyRoundedIcon from '@mui/icons-material/AttachMoneyRounded';
+import ListAltRoundedIcon from '@mui/icons-material/ListAltRounded';
+import PeopleAltRoundedIcon from '@mui/icons-material/PeopleAltRounded';
+import CurrencyExchangeOutlinedIcon from '@mui/icons-material/CurrencyExchangeOutlined';
+
+
+
+
+export default function SubSectionTwo() {
+  return (
+    <div className='u-displayFlex u-Width100 u-Height100  sectionTwoMain '>
+      {/* cardOne 1 */}
+        <div className='base-data-card u-Width25  u-Height100  sectionTwoCardOne '>
+          <div className='u-subtitles u-color-bright-blue'>Movimientos Cartera</div>
+            <div className='card-items u-Width100 u-displayFlex'>
+              <CardOneItems icon={<AttachMoneyRoundedIcon  className='sub-card-icon'/>} descripcion={"Depositos"} value={720.129} valueType={"U$D"}/>
+              <CardOneItems icon={<CurrencyExchangeOutlinedIcon  className='sub-card-icon'/>} descripcion={"Extracciones"} value={750.606} valueType={"U$D"}/>
+              <CardOneItems icon={<ListAltRoundedIcon  className='sub-card-icon'/>} descripcion={"Valor de cuota parte"} value={72.444} valueType={"U$D"}/>
+              <CardOneItems icon={<PeopleAltRoundedIcon  className='sub-card-icon'/>} descripcion={"Participantes Adheridos"} value={499} valueType={""}/>
+            </div>
+        </div>
+
+    {/* cardTwo 2 */}
+        <div className='base-data-card u-Width100 u-Height100 sectionTwoCardTwo'>
+          <div className='cardTwo-content'>
+          <div className='u-subtitles u-color-bright-blue'>Movimientos de Cartera</div>
+        <div className='cardTwo-header'>
+        <div className='cardTwo-header-value'>U$S 5.987,34 <span>Saldo Acumulado</span></div>
+          
+        <div>DowpDownMenu</div>
+        </div>
+        <div className='cardTwo-graph'>Grafico</div>
+        <div className='cardTwo-footer'>
+          <span> <span className='ref-color-1'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> Extracciones </span>
+          <span> <span className='ref-color-2'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> Depositos</span>
+          <span> <span className='ref-color-3'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> Evolucion de la cartera</span>
+        </div>
+
+          </div>
+
+        </div>
+    </div>
+  )
+}
